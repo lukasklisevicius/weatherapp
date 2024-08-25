7018fc6d448a5f69ad82219e13f1ed76c0ed2abf
lukasklisevicius <8200lukas@gmail.com>
2024-08-25 22:53:35 +0300

    update web app

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
-import { StyleSheet} from 'react-native';
+import { StyleSheet, Platform} from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 // import screens
 import Home from './Screens/Home';
 import Result from './Screens/Result';
 import History from './Screens/History';
+import { useEffect } from 'react';
 
 export default function App() {
   // create stacknavigator
@@ -12,6 +13,20 @@ export default function App() {
   // define api
   const API = 'https://api.openweathermap.org/data/2.5/'
   const appID = '&appid=10692b614cde4a27abc3caf08c696dfa&units=metric'
+
+
+  const resizeWindow = () => {
+    if (Platform.OS === 'web') {
+      window.close()
+      window.open('https://lukasklisevicius.github.io/weatherapp/', '_blank', 'width=450,height=950');
+    } else {
+      alert("This feature is only available in the web environment.");
+    }
+  };
+
+  useEffect(() => {
+    resizeWindow()
+}, []);
   // setup render navigator and screens
   return (
     <NavigationContainer>
